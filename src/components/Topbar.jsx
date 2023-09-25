e7656f0304a0d41f381710ced11df20a8b2debaf
davide bragagnolo <davidebraga.99@gmail.com>
2023-09-25 17:56:50 +0200

    modify

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -25,7 +25,7 @@ const Topbar = () => {
       <Container fluid className="px-5">
         <Container className="d-flex px-1 align-items-center">
           <Link to={"/"}>
-            <Navbar.Brand href="#">
+            <Navbar.Brand>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="34"
