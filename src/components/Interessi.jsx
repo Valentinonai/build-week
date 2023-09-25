20224156c2e0b023d048d04e0b3a702d751202bc
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-09-25 15:39:55 +0200

    v1.5

--- src/components/Interessi.jsx
+++ src/components/Interessi.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { Tab, Tabs } from "react-bootstrap";
 
 const Interessi = () => {
+  const [key, setKey] = useState("Aziende");
   return (
     <div className="border border-1 rounded mt-3 p-2 ">
       <h4 className="mt-3">Interessi</h4>
       <Tabs
-        defaultActiveKey="profile"
         id="uncontrolled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
         variant="underline"
         style={{ borderBottom: "1px solid grey" }}
       >
