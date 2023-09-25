b465d8b51fffc6d52f537dc92e0f761399ff17b5
Andrea Cavallo <astarore@gmail.com>
2023-09-25 14:25:43 +0200

    added components to profilepage

--- src/components/MainProfile.jsx
+++ src/components/MainProfile.jsx
@@ -1,7 +1,17 @@
+import Analysis from "./Analysis";
 import MainHeader from "./MainHeader";
+import Resources from "./Resources";
+import Suggested from "./Suggested";
 
 const MainProfile = () => {
-  return <MainHeader />;
+  return (
+    <>
+      <MainHeader />
+      <Suggested />
+      <Analysis />
+      <Resources />
+    </>
+  );
 };
 
 export default MainProfile;
