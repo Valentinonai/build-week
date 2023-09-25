29e42b994e9bf7505f037fd63cf9e82ffa2cad7f
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-09-25 09:31:40 +0200

    v1.0

---
+++ src/redux/store/index.js
@@ -0,0 +1,20 @@
+import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const rootReducer = combineReducers({});
+
+const persistedReducers = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducers,
+  middleware: getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persiStore = persistStore(store);
