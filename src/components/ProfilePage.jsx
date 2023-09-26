a7c200f3e41140fb9370af6917cbe9c9ae29fe75
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-09-26 08:53:07 +0200

    profile fetch generica

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -9,25 +9,20 @@ import "../SideBar/Sidebar.css";
 
 const ProfilePage = () => {
   const dispatch = useDispatch();
-  const currentUser = useSelector(state => state.currentUser.userData);
+  const currentUser = useSelector((state) => state.currentUser.userData);
   const params = useParams();
   useEffect(() => {
-    if (params.id === "me") dispatch(fetchProfileData());
+    dispatch(fetchProfileData(params.id));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     currentUser && (
       <Container fluid="lg">
         <Row>
-          <Col
-            xs={12}
-            xl={9}>
+          <Col xs={12} xl={9}>
             <MainProfile />
           </Col>
-          <Col
-            xs={12}
-            xl={3}
-            id="sidebar">
+          <Col xs={12} xl={3} id="sidebar">
             <Sidebar />
           </Col>
         </Row>
--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -18,48 +18,48 @@ export const MODAL_ON = "MODAL_ON";
 
 export const modalOffAction = () => ({ type: MODAL_OFF, payload: false });
 export const modalOnAction = () => ({ type: MODAL_ON, payload: true });
-export const handleClose = dispatch => dispatch(modalOffAction());
-export const handleShow = dispatch => dispatch(modalOnAction());
+export const handleClose = (dispatch) => dispatch(modalOffAction());
+export const handleShow = (dispatch) => dispatch(modalOnAction());
 
 export const isLoadingTrueAction = () => ({
   type: IS_LOADING_TRUE,
-  payload: true
+  payload: true,
 });
 
 export const isLoadingFalseAction = () => ({
   type: IS_LOADING_FALSE,
-  payload: false
+  payload: false,
 });
 
 export const hasErrorTrueAction = () => ({
   type: HAS_ERROR_TRUE,
-  payload: true
+  payload: true,
 });
 
 export const hasErrorFalseAction = () => ({
   type: HAS_ERROR_FALSE,
-  payload: false
+  payload: false,
 });
 
-export const addErrorMessageAction = string => ({
+export const addErrorMessageAction = (string) => ({
   type: ADD_ERROR_MESSAGE,
-  payload: string
+  payload: string,
 });
 
-export const addCurrentUserDataAction = dataUser => ({
+export const addCurrentUserDataAction = (dataUser) => ({
   type: ADD_CURRENT_USER_DATA,
-  payload: dataUser
+  payload: dataUser,
 });
 
-export const fetchProfileData = () => {
-  return async dispatch => {
+export const fetchProfileData = (param) => {
+  return async (dispatch) => {
     dispatch(isLoadingTrueAction());
     try {
-      const resp = await fetch("https://striveschool-api.herokuapp.com/api/profile/me", {
+      const resp = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${param}`, {
         headers: {
           Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o"
-        }
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o",
+        },
       });
       if (resp.ok) {
         const data = await resp.json();
@@ -76,8 +76,8 @@ export const fetchProfileData = () => {
     }
   };
 };
-export const fetchEditUser = objChanges => {
-  return async dispatch => {
+export const fetchEditUser = (objChanges) => {
+  return async (dispatch) => {
     try {
       const resp = await fetch("https://striveschool-api.herokuapp.com/api/profile/  ", {
         method: "PUT",
@@ -85,8 +85,8 @@ export const fetchEditUser = objChanges => {
         headers: {
           "content-type": "application/json",
           Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o"
-        }
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o",
+        },
       });
 
       if (resp.ok) {
