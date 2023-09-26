d9c83d2f4248e9c308626c2eece722885340113f
Simo095 <simone.dangelo636@gmail.com>
2023-09-26 16:23:09 +0200

    prove per file

--- src/components/Modale.jsx
+++ src/components/Modale.jsx
@@ -6,34 +6,35 @@ import { fetchEditUser, handleClose } from "../redux/action";
 import Dropzone from "react-dropzone";
 
 const Modale = () => {
-  const show = useSelector((state) => state.modal.isShowing);
-  const user = useSelector((state) => state.currentUser.userData);
+  const show = useSelector(state => state.modal.isShowing);
+  const user = useSelector(state => state.currentUser.userData);
   const [nome, setNome] = useState(user.name);
   const [cognome, setCognome] = useState(user.surname);
   const [area, setArea] = useState(user.area);
-  const formImg = new FormData();
+  const [image, setImage] = useState(user.image);
   const dispatch = useDispatch();
+  const handleImage = x => {
+    if (x) {
+      const formImg = new FormData();
 
+      setImage(formImg.append("image", x.target.elements[3].files[0], "image.png"));
+      dispatch(fetchEditUser(image));
+    }
+  };
   return (
     <>
-      <Modal show={show} onHide={() => handleClose(dispatch)}>
+      <Modal
+        show={show}
+        onHide={() => handleClose(dispatch)}>
         <Modal.Header closeButton>
           <Modal.Title>Modifica Presentazione</Modal.Title>
         </Modal.Header>
         <Form
-          onSubmit={(e) => {
+          onSubmit={e => {
             e.preventDefault();
-            console.log(formImg.get("file"), "leggi quest"); //QUI DISPATCHFILE
-            dispatch(
-              fetchEditUser({
-                name: nome,
-                surname: cognome,
-                area: user.area,
-                image: JSON.stringify(formImg),
-              })
-            );
-          }}
-        >
+            handleImage(e);
+            //console.log(e.target.files[0]); //QUI DISPATCHFILE
+          }}>
           <Modal.Body>
             <Form.Group className="mb-3">
               <Form.Label>Nome:</Form.Label>
@@ -43,7 +44,7 @@ const Modale = () => {
                 autoFocus
                 required
                 value={nome}
-                onChange={(e) => {
+                onChange={e => {
                   setNome(e.target.value);
                 }}
               />
@@ -56,7 +57,7 @@ const Modale = () => {
                 placeholder="Scrivi qui..."
                 required
                 value={cognome}
-                onChange={(e) => {
+                onChange={e => {
                   setCognome(e.target.value);
                 }}
               />
@@ -68,32 +69,37 @@ const Modale = () => {
                 rows={1}
                 value={area}
                 placeholder="Scrivi qui..."
-                onChange={(e) => {
+                onChange={e => {
                   setArea(e.target.value);
                 }}
               />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Curriculm</Form.Label>
+              {/* 
               <Dropzone>
                 {({ getRootProps, getInputProps, acceptedFiles }) => (
                   <>
-                    {formImg.append("file", acceptedFiles[0])}
-                    {console.log(formImg)}
                     <div {...getRootProps()}>
                       <input {...getInputProps()} />
-                      <p>Trascina il tuo Curriculm oppure clicca per inserirlo.</p>
+                      <p>Trascina il una immagina oppure clicca per inserirla.</p>
                     </div>
                   </>
                 )}
-              </Dropzone>
+              </Dropzone> */}
+              <Form.Control type="file"></Form.Control>
             </Form.Group>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={() => handleClose(dispatch)}>
+            <Button
+              variant="secondary"
+              onClick={() => handleClose(dispatch)}>
               Close
             </Button>
-            <Button variant="primary" type="submit" onClick={() => handleClose(dispatch)}>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={() => handleClose(dispatch)}>
               Save Changes
             </Button>
           </Modal.Footer>
--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -21,55 +21,55 @@ export const ADD_EXPERIENCES = "ADD_EXPERIENCES";
 
 export const modalOffAction = () => ({ type: MODAL_OFF, payload: false });
 export const modalOnAction = () => ({ type: MODAL_ON, payload: true });
-export const handleClose = (dispatch) => dispatch(modalOffAction());
-export const handleShow = (dispatch) => dispatch(modalOnAction());
-export const addPosts = (data) => ({ type: ADD_POSTS, payload: data });
-export const addExperiences = (data) => ({ type: ADD_EXPERIENCES, payload: data });
+export const handleClose = dispatch => dispatch(modalOffAction());
+export const handleShow = dispatch => dispatch(modalOnAction());
+export const addPosts = data => ({ type: ADD_POSTS, payload: data });
+export const addExperiences = data => ({ type: ADD_EXPERIENCES, payload: data });
 
 //-------------------------PAYLOAD CREATORS-----------------------------
 export const isLoadingTrueAction = () => ({
   type: IS_LOADING_TRUE,
-  payload: true,
+  payload: true
 });
 
 export const isLoadingFalseAction = () => ({
   type: IS_LOADING_FALSE,
-  payload: false,
+  payload: false
 });
 
 export const hasErrorTrueAction = () => ({
   type: HAS_ERROR_TRUE,
-  payload: true,
+  payload: true
 });
 
 export const hasErrorFalseAction = () => ({
   type: HAS_ERROR_FALSE,
-  payload: false,
+  payload: false
 });
 
-export const addErrorMessageAction = (string) => ({
+export const addErrorMessageAction = string => ({
   type: ADD_ERROR_MESSAGE,
-  payload: string,
+  payload: string
 });
 
-export const addCurrentUserDataAction = (dataUser) => ({
+export const addCurrentUserDataAction = dataUser => ({
   type: ADD_CURRENT_USER_DATA,
-  payload: dataUser,
+  payload: dataUser
 });
 
 //------------------FETCH SECTION------------------------
 
 //!---------------FETCH PROFILi---------------------
-export const fetchProfileData = (param) => {
-  return async (dispatch) => {
+export const fetchProfileData = param => {
+  return async dispatch => {
     dispatch(isLoadingTrueAction());
     try {
       console.log(param);
       const resp = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${param}`, {
         headers: {
           Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o",
-        },
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o"
+        }
       });
       if (resp.ok) {
         const data = await resp.json();
@@ -88,22 +88,22 @@ export const fetchProfileData = (param) => {
 };
 
 //!-------------------FETCH MODIFICA USER DATA-----------------------------
-export const fetchEditUser = (objChanges) => {
-  return async (dispatch) => {
+export const fetchEditUser = objChanges => {
+  return async dispatch => {
     try {
       const resp = await fetch("https://striveschool-api.herokuapp.com/api/profile/  ", {
         method: "PUT",
-        body: JSON.stringify(objChanges),
+        body: objChanges,
         headers: {
-          "content-type": "application/json",
           Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o",
-        },
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o"
+        }
       });
 
       if (resp.ok) {
-        console.log(resp);
         const dataChanges = await resp.json();
+        console.log(dataChanges, "datachanges");
+
         dispatch(addCurrentUserDataAction(dataChanges));
       } else {
         throw new Error(resp.status);
@@ -115,16 +115,16 @@ export const fetchEditUser = (objChanges) => {
 };
 
 //!----------------------------FETCH EXPERIENCES------------------------
-export const fetchExperiencies = (id) => {
-  return async (dispatch) => {
+export const fetchExperiencies = id => {
+  return async dispatch => {
     dispatch(isLoadingTrueAction());
     try {
       console.log(id);
       const resp = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${id}/experiences`, {
         headers: {
           Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o",
-        },
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o"
+        }
       });
       if (resp.ok) {
         const data = await resp.json();
@@ -144,15 +144,15 @@ export const fetchExperiencies = (id) => {
 
 //!------------------FETCH POST HOME-------------------------------------
 export const fetchPost = () => {
-  return async (dispatch) => {
+  return async dispatch => {
     try {
       dispatch(isLoadingTrueAction());
       const risp = await fetch("https://striveschool-api.herokuapp.com/api/posts/", {
         headers: {
           "content-type": "application/json",
           Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o",
-        },
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o"
+        }
       });
       if (risp.ok) {
         const data = await risp.json();
