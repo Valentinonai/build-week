57c1fd06de212ad8eb0231434834594a088cb2c1
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-09-27 12:01:40 +0200

    crea post

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Col, Container, Row, Spinner } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchPost } from "../redux/action";
+import {
+  addErrorMessageAction,
+  fetchPost,
+  hasErrorTrueAction,
+  isLoadingFalseAction,
+  isLoadingTrueAction,
+} from "../redux/action";
 import SinglePost from "./SinglePost";
+import FormHome from "./FormHome";
 
 const Home = () => {
   const posts = useSelector((state) => state.post.data);
   const isLoading = useSelector((state) => state.currentUser.isLoading);
   const dispatch = useDispatch();
+  const [profile, setProfile] = useState(null);
+
+  const fetchUser = async () => {
+    try {
+      dispatch(isLoadingTrueAction());
+      const resp = await fetch(`https://striveschool-api.herokuapp.com/api/profile/me`, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzRiOTM3NTJhODAwMTQ1Njg3NWYiLCJpYXQiOjE2OTU2MjY0MjYsImV4cCI6MTY5NjgzNjAyNn0.NFk7YtejuOSYg3g46D2yj7_4nB-6W8xjVATN2MutM4o",
+        },
+      });
+      if (resp.ok) {
+        const data = await resp.json();
+        setProfile(data);
+      } else {
+        dispatch(hasErrorTrueAction());
+        throw new Error(resp.status);
+      }
+    } catch (error) {
+      dispatch(addErrorMessageAction(error.message));
+      console.log("si e' verificato un errore", error.message);
+    } finally {
+      dispatch(isLoadingFalseAction());
+    }
+  };
   useEffect(() => {
     dispatch(fetchPost());
+    fetchUser();
   }, []);
   return isLoading ? (
     <Spinner animation="border" role="status">
@@ -19,7 +52,15 @@ const Home = () => {
     <Container fluid="lg">
       <Row>
         <Col xs={2}></Col>
-        <Col xs={8}>{posts.map((elem, i) => i < 5 && <SinglePost elem={elem} key={`post${i}`} />)}</Col>
+        <Col xs={8}>
+          <FormHome profile={profile} />
+          {posts
+            .filter((elem) => elem.user._id === profile._id)
+            .map((elem, i) => (
+              <SinglePost elem={elem} key={`post${i}`} />
+            ))}
+          {posts.map((elem, i) => i < 5 && <SinglePost elem={elem} key={`post${i}`} />)}
+        </Col>
         <Col xs={2}></Col>
       </Row>
     </Container>
--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,15 +1,19 @@
-import { Card } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 
 const SinglePost = ({ elem }) => {
   return (
-    <Card>
-      <Card.Img variant="top" src={elem.user.image} />
-      <Card.Body>
-        <Card.Title>{elem.userName}</Card.Title>
-        <Card.Text>{elem.text}</Card.Text>
-        <Card.Text>{elem.user.text}</Card.Text>
-      </Card.Body>
-    </Card>
+    <Row>
+      <Col xs={12}>
+        <Card>
+          <Card.Img variant="top" src={elem.user.image} />
+          <Card.Body>
+            <Card.Title>{elem.userName}</Card.Title>
+            <Card.Text>{elem.text}</Card.Text>
+            <Card.Text>{elem.user.text}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 export default SinglePost;
