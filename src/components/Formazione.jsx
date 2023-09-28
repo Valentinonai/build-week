29d951b402a683a534fbada1717ff61b5f0214ca
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-09-28 09:58:36 +0200

    set dinamic experiences

--- src/components/Formazione.jsx
+++ src/components/Formazione.jsx
@@ -10,16 +10,17 @@ import ExperiencesModal from "./ExperiencesModal";
 const Formazione = () => {
   const dispatch = useDispatch();
   const params = useParams();
-  const experiences = useSelector(state => state.addExps.data);
-  const isLoading = useSelector(state => state.currentUser.isLoading);
-  const user = useSelector(state => state.currentUser.userData);
+  const experiences = useSelector((state) => state.addExps.data);
+  const isLoading = useSelector((state) => state.currentUser.isLoading);
+  const user = useSelector((state) => state.currentUser.userData);
   const [image, setImage] = useState(user.image);
   useEffect(() => {
     user && dispatch(fetchExperiencies(params.id === "me" ? user._id : params.id));
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.id]);
   return (
     !isLoading && (
-      <div className="border border-1 rounded mt-2 p-2">
+      <div className='border border-1 rounded mt-2 p-2'>
         <Row>
           <Col xs={8}>
             <div>
@@ -27,28 +28,20 @@ const Formazione = () => {
             </div>
           </Col>
           <Col xs={4}>
-            <div className="d-flex justify-content-end align-items-center">
-              <PlusLg
-                className="me-3"
-                onClick={() => experiencesHandleShow(dispatch)}
-              />
+            <div className='d-flex justify-content-end align-items-center'>
+              <PlusLg className='me-3' onClick={() => experiencesHandleShow(dispatch)} />
             </div>
           </Col>
         </Row>
         <Row>
           {experiences &&
             experiences.map((elem, i) => (
-              <Col
-                xs={12}
-                key={i}>
+              <Col xs={12} key={i}>
                 <SingleExp elem={elem} />
               </Col>
             ))}
         </Row>
-        <ExperiencesModal
-          image={image}
-          setImage={setImage}
-        />
+        <ExperiencesModal image={image} setImage={setImage} />
       </div>
     )
   );
