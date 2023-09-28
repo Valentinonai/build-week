244cb15470f450b582311af24910e8efb9b5e893
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-09-28 17:41:55 +0200

    v4.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import ProfilePage from "./components/ProfilePage";
 import Footer from "./components/Footer";
 import Message from "./components/Message";
@@ -15,22 +15,23 @@ import Rete from "./components/Rete";
 function App() {
   const error = useSelector((state) => state.currentUser.hasError);
   const errorMsg = useSelector((state) => state.currentUser.errorMessage);
+  console.log(window.location.pathname);
   return (
-    <div className='App'>
+    <div className="App">
       {error ? (
-        <Alert variant='danger'>{errorMsg}</Alert>
+        <Alert variant="danger">{errorMsg}</Alert>
       ) : (
         <BrowserRouter>
           <Topbar />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='profile/:id' element={<ProfilePage />} />
-            <Route path='lavoro/' element={<JobsMainPage />} />
-            <Route path='/rete' element={<Rete />} />
-            <Route path='favoritesJobs' element={<FavoritesJobs />} />
+            <Route path="/" element={<Home />} />
+            <Route path="profile/:id" element={<ProfilePage />} />
+            <Route path="lavoro" element={<JobsMainPage />} />
+            <Route path="rete" element={<Rete />} />
+            <Route path="favoritesJobs" element={<FavoritesJobs />} />
           </Routes>
           <Message />
-          <Footer></Footer>
+          {window.location.pathname !== "/lavoro" && window.location.pathname !== "/rete" && <Footer></Footer>}
         </BrowserRouter>
       )}
     </div>
